62a2d23fa6c2267ef489466d8ad9452927ecb8e0
Vishal <Vishal@Meeth>
2022-07-19 11:43:50 +0530

    1st commit

---
+++ Selenium_framework_Maven/src/test/java/selenium_framework_maven/DemoTest.java
@@ -0,0 +1,15 @@
+package selenium_framework_maven;
+
+import org.testng.annotations.Test;
+
+public class DemoTest{
+	@Test
+	public void demoTest() {
+		System.out.println("Test1");
+}
+	@Test
+	public void demoTset1() {
+		System.out.println("Test2");
+	}
+
+}
---
+++ Selenium_framework_Maven/src/test/java/selenium_framework_maven/DemoTest2.java
@@ -0,0 +1,16 @@
+package selenium_framework_maven;
+
+import org.testng.annotations.Test;
+@Test
+public class DemoTest2 {
+	
+		@Test
+		public void demoTest() {
+			System.out.println("Test1");
+	}
+		@Test
+		public void demoTset1() {
+			System.out.println("Test2");
+		}
+	
+}
